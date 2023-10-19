016e528c1b5c4c34bbe661ce84cb908040e90e52
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-10-19 16:58:21 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = domainName();
+  });
+};
+
+let domainName = () => {
+  let pronoun = ["our", "my", "your"];
+  let adj = ["black", "brown", "yellow", "red"];
+  let noun = ["funny", "shy", "happy", "moody", "kind"];
+
+  let names = [
+    "cat",
+    "kangaroo",
+    "cow",
+    "turtle",
+    "alligator",
+    "dog",
+    "hamster",
+    "wildboar"
+  ];
+  let domains = [".it", ".com", ".eu", ".fr", ".de", ".es"];
+
+  function domainNameGenerator(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+
+  let pronounIndex = domainNameGenerator(pronoun);
+  let adjIndex = domainNameGenerator(adj);
+  let nounIndex = domainNameGenerator(noun);
+  let namesIndex = domainNameGenerator(names);
+  let domainsIndex = domainNameGenerator(domains);
+
+  return (
+    pronoun[pronounIndex] +
+    adj[adjIndex] +
+    noun[nounIndex] +
+    names[namesIndex] +
+    domains[domainsIndex]
+  );
 };
