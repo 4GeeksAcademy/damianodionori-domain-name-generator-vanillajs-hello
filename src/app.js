cd30642867f919627884d568ddcb6f694d75192d
Damiano Dionori <107266901+damianodionori@users.noreply.github.com>
2023-10-20 19:33:03 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -5,44 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#domain").innerHTML = domainName();
-  });
-};
+let pronoun = ["our", "my", "your"];
+let adjective = ["black", "brown", "yellow", "red"];
+let noun = ["cat", "kangaroo", "cow", "turtle"];
+let extension = [".it", ".com", ".eu", ".fr", ".de", ".es"];
+let domainNamesArray = [];
+let checker, extLen;
 
-let domainName = () => {
-  let pronoun = ["our", "my", "your"];
-  let adj = ["black", "brown", "yellow", "red"];
-  let noun = ["funny", "shy", "happy", "moody", "kind"];
-
-  let names = [
-    "cat",
-    "kangaroo",
-    "cow",
-    "turtle",
-    "alligator",
-    "dog",
-    "hamster",
-    "wildboar"
-  ];
-  let domains = [".it", ".com", ".eu", ".fr", ".de", ".es"];
-
-  function domainNameGenerator(arr) {
-    return Math.floor(Math.random() * arr.length);
+for (let i = 0; i < pronoun.length; i++) {
+  // Creating nested arrays
+  for (let j = 0; j < adjective.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        domainNamesArray.push(
+          pronoun[i] + adjective[j] + noun[k] + extension[l]
+        ); // If they are not the same, we create a regular domain only
+      }
+    }
   }
-
-  let pronounIndex = domainNameGenerator(pronoun);
-  let adjIndex = domainNameGenerator(adj);
-  let nounIndex = domainNameGenerator(noun);
-  let namesIndex = domainNameGenerator(names);
-  let domainsIndex = domainNameGenerator(domains);
-
-  return (
-    pronoun[pronounIndex] +
-    adj[adjIndex] +
-    noun[nounIndex] +
-    names[namesIndex] +
-    domains[domainsIndex]
-  );
-};
+}
+document.getElementById("list-of-domains").innerHTML = domainNamesArray.join(
+  "<br>"
+); // Sending data to index.html and printing an array line-by-line
